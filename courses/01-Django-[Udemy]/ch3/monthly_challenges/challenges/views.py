bfeff0d331f2478279db3dc4257d16073976c838
Luis G. Carreno Ortiz <carrenolg@gmail.com>
2022-06-23 12:48:54 -0500

    django url, views, path

--- courses/01-Django-[Udemy]/ch3/monthly_challenges/challenges/views.py
+++ courses/01-Django-[Udemy]/ch3/monthly_challenges/challenges/views.py
@@ -1,9 +1,42 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+list_challenges = {
+    "may": "May - Walk for at least 20 minutes every day!",
+    "june": "June - Walk for at least 20 minutes every day!",
+}
 
 # Create your views here.
+def index(request):
+    list_item = ""
+    months = list(list_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_item += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    
+    response_data = f"<ul>{list_item}</ul>"
+    return HttpResponse(response_data)
+
 def january(request):
     return HttpResponse("Eat no met for the entire month!")
 
 def february(request):
     return HttpResponse("Walk for at least 20 minutes every day!")
+
+def monthly_challenge_by_number(request, month):
+    months = list(list_challenges.keys())
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    try:
+        challenge_text = list_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
--- courses/01-Django-[Udemy]/ch3/monthly_challenges/monthly_challenges/urls.py
+++ courses/01-Django-[Udemy]/ch3/monthly_challenges/monthly_challenges/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("challenges/", include("challenges.urls"))
+    path("challenge/", include("challenges.urls"))
 ]
