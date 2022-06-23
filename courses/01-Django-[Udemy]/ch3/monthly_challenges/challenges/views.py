31ae8672e09d67eab941442e5edef56d3ba40f49
Luis G. Carreno Ortiz <carrenolg@gmail.com>
2022-06-23 04:47:51 -0500

    add new urls

--- courses/01-Django-[Udemy]/ch3/monthly_challenges/challenges/views.py
+++ courses/01-Django-[Udemy]/ch3/monthly_challenges/challenges/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def january(request):
+    return HttpResponse("Eat no met for the entire month!")
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes every day!")
